b93fd917ffd2394362d5eaf7acf759b9e4a921fe
Jannis Hajda <jannis.hajda@protonmail.com>
2022-11-03 18:56:18 +0100

    Added author index page #7

---
+++ spec/features/author/index_spec.rb
@@ -0,0 +1,23 @@
+describe "Author index page", type: :feature do
+    it "should render withour error" do
+        visit authors_path
+    end
+
+    it "should have a table with the correct headers" do
+        visit authors_path
+        expect(page).to have_text('Name')
+        expect(page).to have_text('Homepage')
+    end
+
+    it "should display author information" do
+        @alan = FactoryBot.create :author
+        visit authors_path
+        expect(page).to have_text(@alan.name)
+        expect(page).to have_text(@alan.homepage)
+    end
+
+    it "should have a link to the new author page" do
+        visit authors_path
+        expect(page).to have_link('New', href: new_author_path)
+    end
+end
