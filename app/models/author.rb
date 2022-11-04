68c57a669fa99dc7e71cb0cc02339a557b80fb17
Jannis Hajda <jannis.hajda@protonmail.com>
2022-11-04 23:24:14 +0100

    Added edit and delete link to papers index page #14

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@ class Author < ApplicationRecord
     def name
         "#{first_name} #{last_name}"
     end
-
+    
+    has_and_belongs_to_many :papers
     validates :last_name, presence: true, length: { minimum: 1 }
 end
